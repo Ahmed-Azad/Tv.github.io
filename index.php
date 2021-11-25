d90ccab12d1f9dc70bb7c1de6025a3a59c361c91
Ahmed-Azad <73647398+Ahmed-Azad@users.noreply.github.com>
2021-11-25 09:21:22 +0100

    Add files via upload

---
+++ index.php
@@ -0,0 +1,25 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Bigfoot</title>
+    <link rel="stylesheet" href="style.css">
+</head>
+<body>
+    <header>
+        <img id="logo" src="images\BigfootLogo.png">
+    </header>
+    <div class="container">
+<h1 id="head"> Welcome To Bigfoot </h1>
+
+<div>
+
+ <a href="brand.php"> <button id="order" >Order Now </button> </a>
+ <a href="upload.html"> <button id="upload">Upload </button> </a>
+
+</div>
+    </div>
+</body>
+</html>
